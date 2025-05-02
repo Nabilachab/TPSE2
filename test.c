3b86d92e07f4bbde90f6cf79e35d6fa587056d63
Nabilachab <lachabnabila3@gmail.com>
2025-05-02 17:22:21 +0000

    test.c

--- test.c
+++ test.c
@@ -1,102 +1,92 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <semaphore.h>
 #include <time.h>
-
 
 #define NB_BUS_X 5
 #define NB_BUS_Y 4
 #define NB_TRAJETS 10
 #define MAX_TURN 3
 
-// Déclaration du mutex pour les sections critiques
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+int nb_XY = 0, nb_YX = 0;
+int turn_XY = 0, turn_YX = 0;
+int waiting_XY = 0, waiting_YX = 0;
 
-int nb_XY = 0, nb_YX = 0; // Nombre de bus actuellement dans le tunnel dans chaque sens
-int turn_XY = 0, turn_YX = 0; // Nombre de bus consécutifs ayant accédé au tunnel
-int waiting_XY = 0, waiting_YX = 0; // Nombre de bus en attente
-
-// Fonction qui simule un temps de trajet aléatoire entre 1 et 1.5 secondes
 void sleep_random() {
-    usleep((rand() % 501 + 1000) * 1000); // usleep prend des microsecondes
+    usleep((rand() % 501 + 1000) * 1000); // entre 1s et 1.5s
 }
 
-// Fonction pour demander l'accès au tunnel
 void entrer_tunnel(int direction) {
-    // Déclaration de pointeurs vers les variables associées à la direction
-    int opposé, *waiting, *nb, *turn, *turn_opp;
+    int *waiting, *nb, *turn, *turn_opp, *nb_opposé;
+    int max_attempts = 100;
+    int attempts = 0;
 
-    pthread_mutex_lock(&mutex); // Entrée en section critique
+    pthread_mutex_lock(&mutex);
 
-    if (direction == 0) { // Direction X->Y
+    if (direction == 0) {
         waiting = &waiting_XY;
         nb = &nb_XY;
         turn = &turn_XY;
-        opposé = nb_YX;
+        nb_opposé = &nb_YX;
         turn_opp = &turn_YX;
-    } else { // Direction Y->X
+    } else {
         waiting = &waiting_YX;
         nb = &nb_YX;
         turn = &turn_YX;
-        opposé = nb_XY;
+        nb_opposé = &nb_XY;
         turn_opp = &turn_XY;
     }
 
-    (*waiting)++; // Le bus signale qu'il attend
+    (*waiting)++;
 
-    // Tant qu'il y a des bus dans le sens opposé ou que la limite de tour est atteinte
-    while (opposé > 0 || (*turn) >= MAX_TURN) {
-        pthread_mutex_unlock(&mutex); // Sortie temporaire de la section critique
-        usleep(100000); // Attente active (100ms)
-        pthread_mutex_lock(&mutex); // Rentrer pour re-vérifier
-        if (direction == 0) opposé = nb_YX;
-        else opposé = nb_XY;
+    while ((*nb_opposé > 0 || *turn >= MAX_TURN) && attempts < max_attempts) {
+        pthread_mutex_unlock(&mutex);
+        usleep(100000); // attendre 100ms
+        attempts++;
+        pthread_mutex_lock(&mutex);
     }
 
-    (*waiting)--; // Le bus ne fait plus partie des attentes
-    (*nb)++;      // Le bus entre dans le tunnel
-    (*turn)++;    // Incrément du compteur de tour pour son sens
-    *turn_opp = 0; // Réinitialisation du tour de l'autre sens
+    if (attempts >= max_attempts) {
+        (*waiting)--;
+        pthread_mutex_unlock(&mutex);
+        printf("Bus en direction %s abandonne après trop d’attente.\n", direction == 0 ? "X->Y" : "Y->X");
+        return;
+    }
 
-    pthread_mutex_unlock(&mutex); // Sortie de la section critique
+    (*waiting)--;
+    (*nb)++;
+    (*turn)++;
+    *turn_opp = 0;
+
+    pthread_mutex_unlock(&mutex);
 }
 
-// Fonction pour signaler la sortie du tunnel
 void sortir_tunnel(int direction) {
-    pthread_mutex_lock(&mutex); // Entrée en section critique
-
-    if (direction == 0) nb_XY--; // Sortie dans le sens X->Y
-    else nb_YX--;               // Sortie dans le sens Y->X
-
-    pthread_mutex_unlock(&mutex); // Sortie de la section critique
+    pthread_mutex_lock(&mutex);
+    if (direction == 0)
+        nb_XY--;
+    else
+        nb_YX--;
+    pthread_mutex_unlock(&mutex);
 }
 
-// Fonction principale exécutée par chaque thread bus
 void* bus_thread(void* arg) {
-    int id = ((int*)arg)[0]; // ID du bus
-    int ville_depart = ((int*)arg)[1]; // Ville de départ : 0 = X, 1 = Y
+    int* infos = (int*)arg;
+    int id = infos[0];
+    int ville_depart = infos[1];
     char* noms_villes[2] = {"X", "Y"};
 
     for (int i = 1; i <= NB_TRAJETS; i++) {
-        // Trajet aller
+        // Aller
         entrer_tunnel(ville_depart);
         printf("Bus %d de %s : %s -> %s (Trajet %d)\n", id, noms_villes[ville_depart], noms_villes[ville_depart], noms_villes[1 - ville_depart], i);
         sleep_random();
         sortir_tunnel(ville_depart);
 
-        // Trajet retour
+        // Retour
         entrer_tunnel(1 - ville_depart);
         printf("Bus %d de %s : %s -> %s (Trajet %d)\n", id, noms_villes[1 - ville_depart], noms_villes[1 - ville_depart], noms_villes[ville_depart], i);
         sleep_random();
@@ -106,33 +96,27 @@ void* bus_thread(void* arg) {
     return NULL;
 }
 
-// Fonction principale du programme
 int main() {
-    pthread_t threads[NB_BUS_X + NB_BUS_Y]; // Tableau des threads
-    int infos[NB_BUS_X + NB_BUS_Y][2];      // Infos : ID et ville de départ
+    pthread_t threads[NB_BUS_X + NB_BUS_Y];
+    int infos[NB_BUS_X + NB_BUS_Y][2];
 
+    srand(time(NULL));
 
-    srand(time(NULL)); // Initialisation du générateur aléatoire
-
-    // Création des threads pour les bus de la ville X
     for (int i = 0; i < NB_BUS_X; i++) {
-        infos[i][0] = i + 1; // ID
-        infos[i][1] = 0;     // Ville X
+        infos[i][0] = i + 1;
+        infos[i][1] = 0;
         pthread_create(&threads[i], NULL, bus_thread, infos[i]);
     }
 
-    // Création des threads pour les bus de la ville Y
     for (int i = 0; i < NB_BUS_Y; i++) {
-        infos[NB_BUS_X + i][0] = NB_BUS_X + i + 1; // ID
-        infos[NB_BUS_X + i][1] = 1;               // Ville Y
+        infos[NB_BUS_X + i][0] = NB_BUS_X + i + 1;
+        infos[NB_BUS_X + i][1] = 1;
         pthread_create(&threads[NB_BUS_X + i], NULL, bus_thread, infos[NB_BUS_X + i]);
     }
 
-    // Attente de la fin de tous les threads
     for (int i = 0; i < NB_BUS_X + NB_BUS_Y; i++) {
         pthread_join(threads[i], NULL);
     }
 
     return 0;
 }
-
